721f8ea6a5bc3208bf8e7ac5fb96faa669168682
DiegoQ089 <quiroga.diego089@gmail.com>
2024-02-27 16:12:26 -0500

    Update creacion uno.py
    noveno intento

--- creacion uno.py
+++ creacion uno.py
@@ -10,4 +10,4 @@ print("Los primeros diez números de la serie de Fibonacci son:", resultado)
 
 
 print("quiiero entender como funciona la conexion api, otra vez quiero intentar esta frustracion son bastantes intentos")
-print(" elimino esto agrego esto titu AAAAAAAAAAAAAAA")
+print(" elimino esto agrego esto tita sisas")
